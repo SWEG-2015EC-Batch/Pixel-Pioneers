b24aa38ac408a6dbaae74f29457fbd452d8dba9b
Natnael Solomon <149045319+Natnaelx62@users.noreply.github.com>
2023-12-17 19:18:02 +0300

    Update grade_calculator.cpp

--- ETS_1062_15_NATNAEL_SOLOMON/Activity_3.1/grade_calculator/grade_calculator.cpp
+++ ETS_1062_15_NATNAEL_SOLOMON/Activity_3.1/grade_calculator/grade_calculator.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+using namespace std;
+int main()
+{
 
+    int status;
+
+    do
+    {
+        double result;
+        cout << "\venter your total result (100%): ";
+        cin >> result;
+
+    
+        cout << "____________________________\n\v";
+        if (result >= 90)
+        {
+            cout << "Your grade is : A+" << endl;
+        }
+        else if (result >= 80)
+        {
+            cout << " Your grade is : A" << endl;
+        }
+        else if (result >= 75)
+        {
+            cout << "Your grade is : B+" << endl;
+        }
+        else if (result >= 60)
+        {
+            cout << "Your grade is : B" << endl;
+        }
+        else if (result >= 55)
+        {
+            cout << "Your grade is : C+" << endl;
+        }
+        else if (result >= 45)
+        {
+            cout << "Your grade is : C" << endl;
+        }
+        else if (result >= 30 && result <= 45)
+        {
+            cout << "Your grade is : D" << endl;
+        }
+
+        else
+        {
+            cout << "Your grade is : F " << endl;
+        }
+        cout << "____________________________\n\v";
+        
+        
+
+        cout << "Enter 0 to leave or any other number to continue: ";
+        cin >> status;
+    } while (status);
+
+    return 0;
+}
