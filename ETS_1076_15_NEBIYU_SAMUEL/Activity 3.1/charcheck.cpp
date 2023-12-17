1fccef2794d3f9a35a4baad5d22f185b7b8cecad
Neb-iyu <149391567+Neb-iyu@users.noreply.github.com>
2023-12-16 23:55:07 -0800

    Update charcheck.cpp

--- ETS_1076_15_NEBIYU_SAMUEL/Activity 3.1/charcheck.cpp
+++ ETS_1076_15_NEBIYU_SAMUEL/Activity 3.1/charcheck.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
+//checks if a charater is digit, lowercase, uppercase or special character
+int main()
+{
+    char character;
+    cout << "\vEnter a single character: ";
+    cin >> character;
+    cout << endl;
 
-
-int main() {
-    char ch;
-
-    cout << "Enter a character: ";
-    cin >> ch;
-
-    if(islower(ch))
-        cout << "The character is lowercase." << endl;
-    else if(isupper(ch))
-        cout << "The character is uppercase." << endl;
-    else
-        cout << "The character is neither uppercase nor lowercase." << endl;
+    if (isalpha(character)) //checks whether the character is a uppercase or lowercase
+    {
+        if (isupper(character))
+        {
+            cout << character << " is an uppercase letter.\n\v";
+        }
+        else
+        {
+            cout << character << " is a lowercase lettr.\n\v";
+        }
+    }
+    else if (isdigit(character)) //checks if it's a digit
+    {
+        cout << character << " is a digit.\n\v";
+    }
+    else //the final stop if the above conditions are not true
+    {   
+        cout << character << " is a special character.\n\v";
+    }
 
     return 0;
 }
