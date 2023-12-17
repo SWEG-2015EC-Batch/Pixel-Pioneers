e82e594a5c74cd848709c5202faf373437f9acec
Natnael Solomon <149045319+Natnaelx62@users.noreply.github.com>
2023-12-17 10:41:18 +0300

    Update simple_calculator.cpp

--- ETS_1062_15_NATNAEL_SOLOMON/Activity_3.1/simple_calculator/simple_calculator.cpp
+++ ETS_1062_15_NATNAEL_SOLOMON/Activity_3.1/simple_calculator/simple_calculator.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    char operator1;
+    double num1, num2;
+    int status;
+
+    do
+    {
+        cout << "\vEnter the first number: ";
+        cin >> num1;
+        cout << "\vEnter an operator: ";
+        cin >> operator1;
+        cout << "\vEnter the second number: ";
+        cin >> num2;
+        cout << endl;
+
+        switch (operator1)
+        {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '/':
+            if (num2 == 0)
+            {
+                cout << "Error! undefined." << endl;
+            }
+            else
+            {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            }
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        default:
+            cout << "Error! invalid operator.\v";
+            break;
+        }
+
+        cout << "\vEnter 0 if you want to terminate the program or enter any other number to continue: ";
+        cin >> status;
+        cout << endl
+             << endl;
+
+    } while (status);
+}
