13390849942145f31f882641acf7a448bf2189aa
Lealem-Addis <149386501+Lealem-Addis@users.noreply.github.com>
2023-12-18 00:39:50 +0300

    Add files via upload

---
+++ ETS0812_15Lealem_Addis/Activity 3.1/pattern.cpp
@@ -0,0 +1,166 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+
+    cout<<"**********************************************************" <<endl;
+    cout<<"********************choice********************************" <<endl;
+    cout<<"**********************************************************" <<endl;
+ 
+    cout<<"1.rectangle "<<endl;
+    cout<<"2.full pyramid "<<endl;
+    cout<<"3.half pyramid "<<endl;
+    cout<<"4. hollow full pyramid "<<endl;
+    cout<<"5. inverted half pyramid "<<endl;
+    cout<<"6. hollow rectangle "<<endl;
+    cout<<"7. hollow inverted half rectangle"<<endl;
+    cout<<"8. half number pyramid"<<endl;
+
+
+    int choice;
+    cin>>choice;
+    cout<<"Enter the number of * : ";
+    int n;
+    cin >>n;
+
+    switch(choice)
+    {
+        case 1:
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                cout<<"* ";
+            }
+        cout<<endl;
+
+        }
+        break;
+
+        case 2:
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                cout << " ";
+            }
+
+            for(int k=1; k<=(2*i-1); k++)
+            {
+                cout << "*";
+            }
+
+            cout << "\n";
+        }
+
+        break;
+        case 3:
+        for(int i=1; i<=n; i++)
+        {
+            for( int j=1; j<=i; j++)
+            {
+                cout<<"* " ;
+            }
+            cout<<endl;
+        }
+        break;
+        case 4:
+    
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                cout << " ";
+            }
+
+            if(i == 1 || i == n)
+            {
+                for(int k=1; k<=(2*i-1); k++)
+                {
+                    cout << "*";
+                }
+            }
+            else
+            {
+                cout << "*";
+                for(int k=1; k<=(2*i-3); k++)
+                {
+                    cout << " ";
+                }
+                cout << "*";
+            }
+
+            cout << "\n";
+        }
+        break;
+
+        case 5:
+        for(int i=n; i>=1; i--)
+        {
+            for( int j=i; j>0; j--)
+            {
+                cout<<"* " ;
+            }
+            cout<<endl;
+        }
+        break;
+
+        case 6:
+        int m;
+        cout << "Enter the number of rows (greater than 2): ";
+        cin >> m;
+        cout<<endl;
+        for( int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(i==1 || i==m || j==1 || j==n)
+                {
+                    cout << " "<< "* ";
+                }
+                else
+                {
+                    cout<<"   ";
+                }
+            }
+            cout<<endl;
+        }
+        break;
+
+        case 7:
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        for (int i = n; i > 1; i--)
+        {
+            cout<<"*";
+            for (int j = 0; j < i - 3; j++)
+            {
+                cout<<" ";
+            }
+            if (i!=2)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+
+        break;
+
+        case 8:
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        break;
+    }
+   
+    return 0;
+}
