6882049d571519f5b8b89e0d5f608f41f187c361
Neb-iyu <149391567+Neb-iyu@users.noreply.github.com>
2024-01-14 09:13:42 -0800

    Create Favorite_TV_Show.cpp

---
+++ ETS_1076_15_NEBIYU_SAMUEL/Activity 4.2/String/Favorite_TV_Show.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string favorite1 = "Atlanta";
+    char favorite2[50]; // Assuming a maximum length for the program name
+    strcpy(favoriteProgram2, "Samurai Champloo");
+
+    cout<<"My two favorite television programs are:\n";
+    cout<<"1. "<<favorite1<<endl;
+    cout<<"2. "<<favorite2;
+}
