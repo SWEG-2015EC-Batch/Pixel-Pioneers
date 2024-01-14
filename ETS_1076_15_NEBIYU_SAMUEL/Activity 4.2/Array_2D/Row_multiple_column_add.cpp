a0be0eaf9b31a034179760ad6f67cb07e635face
Neb-iyu <149391567+Neb-iyu@users.noreply.github.com>
2024-01-14 07:37:37 -0800

    Update and rename Row_add_column_multiple.cpp to Row_multiple_column_add.cpp

--- ETS_1076_15_NEBIYU_SAMUEL/Activity 4.2/Array_2D/Row_multiple_column_add.cpp
+++ ETS_1076_15_NEBIYU_SAMUEL/Activity 4.2/Array_2D/Row_multiple_column_add.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-
+#define MAX 3
 using namespace std;
 
 int main(){
     
-    int array[3][3] = {};
-    int col_sum[3] = {};
-    int row_pro[3] = {};
+    int array[MAX][MAX] = {};
+    int col_sum[MAX] = {};
+    int row_pro[MAX] = {};
     int sum = 0, product = 1;
 
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < MAX; i++){
         cout<<"Enter the elements of row "<<i+1<<" space separated: ";
         for(int j = 0; j < 3; j++){
             cin>>array[i][j];
         }
     }
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+    for(int i = 0; i < MAX; i++){
+        for(int j = 0; j < MAX; j++){
             sum += array[i][j];
             product *= array[j][i];
         }
@@ -27,7 +27,7 @@ int main(){
         sum = 0;
     }
 
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < MAX; i++){
         cout<<"Sum of column "<<i + 1<<" is: "<<col_sum[i]<<endl;
         cout<<"Product of row "<<i + 1<<" is: "<<row_pro[i]<<endl;
     }
