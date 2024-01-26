666c313ece1bc65efe05a09ac9ae75cd184c8691
Natnael Solomon <149045319+Natnael-solomon@users.noreply.github.com>
2024-01-26 16:50:33 +0300

    Update 1.2
    Additions
    -More comments
    -Allowed the user to search about the developers
    Changes
    -Formatted the output to be more user friendly
    -Made the tabular summary of the month to be included in the main menu with the product and storekeeper records
    -Changed the products to p in the tabular format
    Fixes
    -Corrected the tabular format's rows to represent products and the columns to show storekeepers

--- INVENTORY MANAGEMENT SYSTEM/InventoryMS.cpp
+++ INVENTORY MANAGEMENT SYSTEM/InventoryMS.cpp
@@ -1,184 +1,253 @@
 //Implements an inventory management system
 
 #include <iostream>
+
 #define DAYS 30
 using namespace std;
-int main()
-{
-    int warehouse[4][DAYS] = {0}, products[5][DAYS] = {0}, quantity, n, x, table[5][4] = {0};
-    
-    for (int i = 0; i < DAYS; i++)
-    {
-        cout<<"Day "<<i+1<<endl;        
+int main() {
+        int warehouse[4][DAYS] = {
+                0
+        }, products[5][DAYS] = {
+                0
+        }, quantity, warehousenum, producttype, choice, table[5][4] = {
+                0
+        };
+        int total_product = 0, total_invtry = 0;
+        float bonus[4];
 
-        for (int j = 0; j < 4; j++)
-        {
-            //Input the warehouse where the product was stored at
-            do
-            {
-                cout<<"Please enter the warehouse number(cannot exceed 4): ";
-                if (!(cin>>n))
-                {
-                    cout<<"Invalid Input\n";
-                    return 1;
-                }
-            } while (n > 4);
 
-            //Input the product stored
-            do
-            {
-                cout<<"Please enter the product stored today(cannot exceed 5): ";
-                if (!(cin>>x))
-                {
-                    cout<<"Invalid Input\n";
-                    return 1;
+
+        cout << "\v\v\v<==============================================================================>\n";
+        cout << "           I N V E N T O R Y     M A N A G E M E N T     S Y S T E M             \n";
+        cout << "<==============================================================================>\n\v";
+
+        cout << " \t\t\t\tW E L C O M E !\n\v";
+
+        for (int i = 0; i < DAYS; i++) {
+                cout << "\nDay " << i + 1 << endl << endl;
+
+                for (int j = 0; j < 5; j++) {
+
+                        //Input the warehouse where the product was stored at
+                        do {
+                                cout << "Please enter the warehouse number (1-4): ";
+                                if (!(cin >> warehousenum)) {
+                                        cout << "Invalid Input\n";
+                                        return 1;
+
+                                }
+                        } while ((warehousenum > 4) || (0 >= warehousenum));
+
+                        //Input the product stored
+                        do {
+                                cout << "Please enter the product type received (1-5): ";
+                                if (!(cin >> producttype)) {
+                                        cout << "Invalid Input\n";
+                                        return 1;
+                                }
+                        } while ((producttype > 5) || (0 >= producttype));
+
+                        cout << "Please enter the quantity of the products recieved: ";
+                        if (!(cin >> quantity)) {
+                                cout << "Invalid Input\n";
+                                return 1;
+                        }
+                        cout << "_______________________________________________________________________\n\v";
+
+                        warehouse[warehousenum - 1][i] = quantity;
+                        products[producttype - 1][i] += quantity;
+                        table[producttype - 1][warehousenum - 1] += quantity;
+                        
                 }
-            } while (x > 5);
-            
-            cout<<"Please enter the quantity of the products recieved: ";    
-            if (!(cin>>quantity))
-            {
-                cout<<"Invalid Input\n";
+
+        }
+
+        mainmenu:
+                cout << "\v\v===================> M E N U <===================\n\v";
+        cout << "\t1.....Tabular summary of the month.\n";
+        cout << "\t2.....Products (1-5) record.\n";
+        cout << "\t3.....Storekeepers (1-4) record.\n";
+        cout << "\t4.....About.\n";
+        cout << "\t5.....Exit.\n";
+        cout << "\v======================><========================\n\v";
+        cout << "Enter yout choice here (1-4): ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+                //Print the table format of the inventory
+                cout << "\v-----------------------------------------------------------------------------------------------------------------------------\n";
+                cout << "\n                      T A B U L A R    S U M M A R Y    O F   T H E    T O T A L    I N V E N T O R Y   \n";
+                cout << "\v=============================================================================================================================\n";
+                cout << "                     |Storekeeper 1\t|Storekeeper 2\t\t|Storekeeper 3\t\t|Storekeeper 4\t\t|Total sales\n";
+                cout << "-----------------------------------------------------------------------------------------------------------------------------\n";
+
+                for (int i = 0; i < 5; i++) {
+                        //Prints the total inventory per product
+
+                        cout << "\n\vProduct " << i + 1 << " \t     |";
+                        for (int j = 0; j < 4; j++) {
+                                cout << table[i][j] << "\t\t\t|";
+                                total_product += table[i][j];
+                        }
+                        cout << total_product << endl;
+                        total_product -= total_product;
+                }
+
+                cout << "\n\vT. Inventory\t     |";
+
+                //Prints the total inventory per salesperson
+
+                for (int i = 0; i < 4; i++) {
+
+                        for (int j = 0; j < 5; j++) {
+                                total_invtry += table[j][i];
+                        }
+                        cout << total_invtry << "\t\t\t|";
+                        bonus[i] = (float) 0.05 * total_invtry;
+                        total_invtry -= total_invtry;
+
+                }
+                cout << "|";
+
+                cout << "\n\vBonus Payment\t     |";
+                //prints the bonus paymnet
+
+                for (int i = 0; i < 4; i++) {
+
+                        cout << bonus[i] << "\t\t        |";
+                }
+                cout << "|";
+                cout << "\n-----------------------------------------------------------------------------------------------------------------------------\n";
+                do {
+
+                        cout << "\n\vEnter 1 to go back to the main menu OR 0 to exit the program: ";
+                        if (!(cin >> choice)) {
+                                cout << "Invalid input\n";
+                                return 1;
+
+                        }
+                        if (choice == 1) {
+                                goto mainmenu;
+                        } else if (choice == 0) {
+                                cout << "\v\nHave a nice day!\n\v";
+                                goto exit;
+                        }
+
+                } while ((choice > 1) || (choice < 0));
+
+                break;
+
+        case 2:
+                do {
+                        cout << "\vEnter 1-5 to search each product type OR 6 for the entire products record: ";
+                        cin >> producttype;
+                        cout << endl;
+
+                } while ((producttype > 6) || (0 > producttype));
+
+                //Prints the entire record
+                if (producttype == 6) {
+                        cout << "Days\t\t\tQuantities per product (P)\n";
+                        cout << "\t\t________________________________________\n";
+                        cout << "\t\tP1\t|P2\t|P3\t|P4\t|P5\n";
+                        for (int i = 0; i < DAYS; i++) {
+                                cout << "Day " << i + 1 << "\t\t|";
+                                for (int j = 0; j < 5; j++) {
+                                        cout << products[j][i] << "\t|";
+                                }
+                                cout << endl;
+                        }
+                }
+                //Prints a single record
+                else {
+                        cout << "\v------------------------------\n\v";
+                        cout << "Days\t|Quantity of product " << producttype << endl;
+                        cout << "\v------------------------------\n\v";
+                        for (int i = 0; i < DAYS; i++) {
+                                cout << "Day " << i + 1 << "\t|";
+                                cout << products[producttype][i] << endl;
+                        }
+                        cout << "\v------------------------------\n\v";
+                }
+
+                do {
+                        cout << "\v\nPress 0 to exit or 1 to go back to the main menu: \n";
+                        cin >> choice;
+                } while ((choice < 0) || (choice > 1));
+                if (choice == 0) {
+                        cout << "Have a nice day!\n";
+                        goto exit;
+                } else if (choice == 1) {
+                        goto mainmenu;
+                }
+
+                break;
+
+        case 3:
+                do {
+                        cout << "\vEnter 1-4 for single storekeeper record OR 5 for all storekeepers: ";
+                        cin >> warehousenum;
+
+                } while ((warehousenum > 5) || (0 > warehousenum));
+                //Print the entire record
+                if (warehousenum == 5) {
+                        cout << "Days\t   Quantities per Warehouse\\Storekeeper\n";
+                        cout << "___________________________________________\n";
+                        cout << "\t\tS1\t|S2\t|S3\t|S4\n";
+                        for (int i = 0; i < DAYS; i++) {
+                                cout << "Day " << i + 1 << "\t\t|";
+                                for (int j = 0; j < 4; j++) {
+                                        cout << warehouse[j][i] << "\t|";
+                                }
+                                cout << endl;
+                        }
+                }
+                //Print a single record
+                else {
+                        cout << "\v==========================================\n";
+                        cout << "Days\t|Records of storekeeper " << warehousenum << endl;
+                        cout << "---------------------------------\n";
+                        for (int i = 0; i < DAYS; i++) {
+                                cout << "Day " << i + 1 << "\t|";
+                                cout << warehouse[warehousenum - 1][i] << endl;
+                        }
+                }
+                cout << "===========================================\n";
+                do {
+                        cout << "\v\nPress 0 to exit or 1 to go back to the main menu: \n";
+                        cin >> choice;
+                } while ((choice < 0) || (choice > 1));
+                if (choice == 0) {
+                        cout << "Have a nice day!\n";
+                        goto exit;
+                } else if (choice == 1) {
+                        goto mainmenu;
+                }
+                break;
+        case 4:
+                cout << "\v\vPixel Pioneers -- 2024 G.C\n\v\v";
+                do {
+                        cout << "\v\nPress 0 to exit or 1 to go back to the main menu: \n";
+                        cin >> choice;
+                } while ((choice < 0) || (choice > 1));
+                if (choice == 0) {
+                        cout << "Have a nice day!\n";
+                        goto exit;
+                } else if (choice == 1) {
+                        goto mainmenu;
+                }
+
+                break;
+        case 5:
+                cout << "\vHave a nice day!\n\v";
+                return 0;
+                break;
+        default:
+                cout << "Invalid choice, try again!\n";
                 return 1;
-            }
-            
-            warehouse[n-1][i] = quantity;
-            products[x-1][i] += quantity;
-            table[x-1][n-1] += quantity;
-        }
-    }
-    //Print the table format of the inventory
-    float bonus[4];
-    cout<<"\nWarehouses\t\t\t\t\tProducts\n";
-    cout<<"\t\t________________________________________________________________________________________\n";
-    cout<<"\t\tProduct 1\t|Product 2\t|Product 3\t|Product 4\t|Product 5\t|Tot\\w.house\n";
-    cout<<"____________________________________________________________________________________________________\n";
-    for (int i = 0; i < 4; i++)
-    {
-        int total_row = 0;
-        cout<<"Warehouse "<<i+1<<"\t|";
-        for (int j = 0; j < 5; j++)
-        {
-            cout<<table[i][j]<<"\t\t|";
-            total_row += table[i][j];
-        }
-        //Print the total inventory per warehouse
-        cout<<total_row<<endl;
-        //Calculate the bonus of the warehouse
-        bonus[i] = (float)5 / 100 * total_row;
-    }
-    cout<<"Tot\\product\t|";
-    //Print the total inventory per product
-    for (int i = 0; i < 5; i++)
-    {
-        int total_column = 0;
-        for (int j = 0; j < 4; j++)
-        {
-        total_column += table[j][i];
-        }
-        cout<<total_column<<"\t\t|";
-    }
-    cout<<"\n____________________________________________________________________________________________________\n";
-    //Print out the bonus
-    for (int i = 0; i < 4; i++)
-    {
-        cout<<"Shopkeeper "<<i+1<<"'s is"<<bonus[i]<<endl;
 
-bool done = false;
-    do
-    {
-        cout<<"If you would like to search a product's record press 1.\n";
-        cout<<"If you would like to search a storekeeper's record press 2\n If you would like to exit press any other key\n";
-        if ((!cin>>n))
-        {
-            cout<<"Invalid Input";
-            return 1;
         }
-        switch (n)
-        {
-            case 1:
-            do
-            { 
-                cout<<"Which product would you like to search for (Press 1-5 for single search 6 for the entire product)";
-                if (!cin>>n);
-                {
-                    cout<<"Invalid Input";
-                    return 1;
-                }
-            } while (n > 6);
-            
-           //Print the entire record
-            if (n == 6)
-            {
-                cout<<"Days\t\t\tQuantities per product\n";
-               cout<<"\t\t_________________________________________________________\n";
-                cout<<"\t\tProduct 1\t|Product 2\t|Product 3\t|Product 4\t|Product 5\n";
-                for (int i = 0; i < DAYS; i++)
-                {
-                    cout<<"Day "<<i+1<<"\t\t|";
-                    for (int j = 0; j < 5; j++)
-                    {
-                        cout<<products[j][i]<<"\t|";
-                    }
-                    cout<<endl;
-                }
-            }
-            //Print a single record
-            else
-            {                
-                cout<<"Days\t|Quantity of product "<<n<<endl;
-                for (int i = 0; i < DAYS; i++)
-                {
-                    cout<<"Day "<<i+1<<"\t|";
-                    cout<<products[n][i]<<endl;
-                }
-            }
-            break;
-            //Print the storekeeper's record
-            case 2:
-            do
-            { 
-                cout<<"Which storekeeper/s would you like to search for? (Press 1-4 for single search 5 for all storekeepers)";
-                if (!cin>>n);
-                {
-                    cout<<"Invalid Input";
-                    return 1;
-                }
-            } while (n > 5);
-            //Print the entire record
-            if (n == 5)
-            {
-                cout<<"Days\t\t\tQuantities per Warehouse\\Storekeeper\n";
-                cout<<"\t\t______________________________________________\n";
-                cout<<"\t\tStorekeeper 1\t|Storekeeper 2\t|Storekeeper 3\t|Storekeeper 4\n";
-                for (int i = 0; i < DAYS; i++)
-                {
-                    cout<<"Day "<<i+1<<"\t\t|";
-                    for (int j = 0; j < 4; j++)
-                    {
-                        cout<<warehouse[j][i]<<"\t|";
-                    }
-                    cout<<endl;
-                }
-            }
-            //Print a single record
-            else
-            {                
-                cout<<"Days\t|Quantity of storekeeper "<<n<<endl;
-                for (int i = 0; i < DAYS; i++)
-                {
-                    cout<<"Day "<<i+1<<"\t|";
-                    cout<<warehouse[n][i]<<endl;
-                }
-            }
-            break;
-            default:
-            done = true;
-            break;
-        }
-    } while (!done);
-    }
-  
-  return 0;
-
+        exit:
+                return 0;
 }
